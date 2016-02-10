48a33397d82ad47b484f3485ce1a31e99e75b979
simplon-jeromem <the-3-man@live.fr>
2016-02-10 10:23:47 +0100

    premier commit

---
+++ json.php
@@ -0,0 +1,52 @@
+<?php 
+ try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=simplonsite;charset=utf8', 'root', 'passe');
+
+    } catch ( Exception $e ){
+        die('Erreur : '.$e->getMessage() );
+    }
+$eleve=[];
+    $requete="SELECT * 
+FROM  `lien` 
+INNER JOIN apprenant ON apprenant.id = lien.id";
+$resultats = $bdd->query($requete);
+ while( $users = $resultats->fetch()){
+     $tableau=[
+         "nom"=>$users["nom"],
+         "prenom"=>$users["prenom"],
+         "gif"=>$users["gif"],
+         "gif2"=>$users["photo"],
+         "photoMin"=>$users["photoMin"],
+         "presentation"=>$users["description"],
+         "twitter"=>$users["twitter"],
+         "linkedin"=>$users["linked"],
+         "codepen"=>$users["codepen"],
+         "portefolio"=>$users["siteperso"],
+         "email"=>$users["mail"],
+         "cv"=>$users["cv"],
+        "competences"=>[]
+     ];
+     $id=$users["id"];
+     $id=$bdd->quote($id);
+     $requete2="SELECT*
+FROM  `techno` 
+INNER JOIN competences ON techno.id = competences.idt
+WHERE ida=$id";
+     $resultats2 = $bdd->query($requete2);
+ while($profil = $resultats2->fetch()){
+    $competences=[
+      "nom"=>$profil["techno"],
+       "niveau"=>$profil["niveau"]
+    ];
+     array_push($tableau["competences"],$competences);
+     
+ };
+     
+     
+     array_push($eleve,$tableau);
+ }; 
+echo json_encode($eleve);
+?>
+
+
